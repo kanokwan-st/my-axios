3b0fccc0bbc4bcd06165f986944f1067b659a157
kanokwan-st <ganokwn123@gmail.com>
2025-04-01 01:24:43 +0700

    feat(files): create cat fact app

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,30 @@
-import { AxiosAsyncAwait } from "./components/AxiosAsyncAwait";
-import { AxiosThenCatch } from "./components/AxiosThenCatch";
-import { FetchPromise } from "./components/FetchPromise";
-import { APIExplorer } from "./components/APIExplorer";
-import { FetchAsyncAwait } from "./components/FetchAsyncAwait";
-// import { APIExplorer2 } from "./components/APIExplorer2";
+import { CatAxiosAsyncAwait } from "./components/CatAxiosAsyncAwait";
+import { CatStuff } from "./components/CatStuff";
 
 export default function App() {
   return (
-    <div className="min-h-screen flex justify-center bg-blue-950">
-      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
-          First Meet with Axios
-        </h1>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <ul className="list-inside list-disc flex-1">
-            <span className="font-semibold">Examples:</span>
-            <li>Axios with Async/Await</li>
-            <li>Axios with Then/Catch</li>
-            <li>Fetch Async/Await</li>
-            <li>Fetch Then/Catch</li>
-            <li>API Explorer</li>
-          </ul>
-        </section>
-
-        <section className="w-full p-5 bg-amber-100 flex">
-          <AxiosAsyncAwait />
-        </section>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <AxiosThenCatch />
-        </section>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <FetchAsyncAwait />
-        </section>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <FetchPromise />
-        </section>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <APIExplorer />
-        </section>
-        {/* <section className="w-full p-5 bg-amber-100 flex">
-          <APIExplorer2 />
-        </section> */}
-      </div>
+    <div>
+      <CatStuff />
     </div>
+
+
+    // <div className="min-h-screen flex justify-center bg-blue-950">
+    //   <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
+    //     <h1 className="w-full p-6 bg-amber-100 font-extrabold">
+    //       First Meet with Axios
+    //     </h1>
+    //     <section className="w-full p-5 bg-amber-100 flex">
+    //       <ul className="list-inside list-disc flex-1">
+    //         <span className="font-semibold">Examples:</span>
+    //         <li>Axios with Async/Await</li>
+    //       </ul>
+    //     </section>
+
+    //     <section className="w-full p-5 bg-amber-100 flex">
+    //       <CatAxiosAsyncAwait />
+    //     </section>
+
+    //   </div>
+    // </div>
   );
 }
---
+++ src/components/CatStuff.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export function CatStuff() {
+    const [catFact, setCatFact] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [catWord, setCatWord] = useState(false);
+
+    useEffect(() => {
+        async function getCatFact() {
+            try {
+                const res = await axios.get("https://catfact.ninja/fact");
+                setCatFact(res.data.fact);
+                console.log(res)
+            } catch(error) {
+                setError(error);
+                console.log(error);
+            } finally {
+                setLoading(false);
+                setCatWord(false)
+            }
+        };
+
+        getCatFact();
+    }, [catWord])
+
+    function catButton() {
+        setCatWord(true);
+    }
+
+    return (
+        <div className="flex flex-col items-center h-screen bg-[#1b99d3]">
+            <div className="flex flex-col items-center w-[40%] my-10 p-4 bg-[#f6cd4b] text-gray-800 rounded-2xl">
+                <h1 className='font-bold text-3xl'>The Kitty Said...</h1>
+                {loading && <p>loading...</p>}
+                {error && <p>There is an error.</p>}
+                <p className='italic font-semibold mt-2 text-center '>"{catFact}"</p>
+                <img src="../../public/cat.jpg" alt="cat" className='w-50 h-40 rounded-2xl mt-4'/>
+                <button onClick={catButton} className="w-50 py-2 mt-4 bg-[#43a591] text-white font-bold rounded-4xl hover:bg-[#477a70fe] hover:cursor-pointer">More Cat Words</button>
+            </div>
+        </div>
+    )
+}
